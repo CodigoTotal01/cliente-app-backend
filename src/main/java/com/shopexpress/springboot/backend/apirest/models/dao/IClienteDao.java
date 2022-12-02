d49909f445b7ab713ec3dec86e13ee5f320047d3
Palacios Tarrillo Christian <85316618+PalaciosTarrilloChristian@users.noreply.github.com>
2022-12-02 06:58:33 -0500

    Inicio de Paginacion

--- src/main/java/com/shopexpress/springboot/backend/apirest/models/dao/IClienteDao.java
+++ src/main/java/com/shopexpress/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -4,6 +4,8 @@ import com.shopexpress.springboot.backend.apirest.models.entity.Cliente;
 import org.springframework.data.repository.CrudRepository;
 
 
+//no util para paginacion -> CrudRepository<Cliente,Long
+
 //?Extiende de un servicio que facilita realizar CRUD -> nombre entidad y el tipo de dato del id
 public interface IClienteDao extends CrudRepository<Cliente,Long> {}
 //! implmentandolo tenemos un bean
